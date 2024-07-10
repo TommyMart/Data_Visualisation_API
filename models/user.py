3ed505629f26a8401f119f59942c5ceaf0fdafb5
TommyMart <15423@coderacademy.edu.au>
2024-07-10 11:17:47 +0930

    added Post model and connected user and post

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 # "user" table model and schema
 
 from init import db, ma
+from marshmallow import fields
 
 # create model class extended from the sqlalchemy model class 
 class User(db.Model):
@@ -19,9 +20,20 @@ class User(db.Model):
     # is_admin column - boolean value and cannot be null
     is_admin = db.Column(db.Boolean, default=False)
 
+    # connects the two variables user and post from posts.py
+    # connects to the Post model and user field
+    posts = db.relationship("Post", back_populates="user")
+
+
 # schema instance from marshmallow - convert db objects to python objects
 # and python objects to db objects
 class UserSchema(ma.Schema):
+
+    # a user can have zero or more posts so it is a list, each object in 
+    # the field is a nested object. Use PostSchema to serialise and deserialise,
+    posts = fields.List(fields.Nested("PostSchema", exclude=["user"]))
+
+
     class Meta:
         fields = ("id", "name", "email", "password", "is_admin")
 
@@ -29,6 +41,7 @@ class UserSchema(ma.Schema):
 # user_schema object set to call UserSchema class
 # we do not want to pass the password back to the user so we exclude it
 # to handle a single user object
+# must exclude user so it does not make a never ending loop
 user_schema = UserSchema(exclude=["password"])
-# # to handle a list user objects
+# to handle a list user objects
 users_schema = UserSchema(many=True, exclude=["password"])
